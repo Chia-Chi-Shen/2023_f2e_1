3ae80a1aceb0705488be89e3ce4ce6a028859259
Chia-Chi-Shen <musicsusanshen@gmail.com>
2023-11-19 15:33:24 +0800

    Updates

---
+++ precache-manifest.0d4f8120e897cf101652964e2d599700.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad63595482d71d6ff29ba6b8612d2da",
+    "url": "/2023_f2e_1/index.html"
+  },
+  {
+    "revision": "966bbb4127bf45557e39",
+    "url": "/2023_f2e_1/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "0e449d00275552e235f0",
+    "url": "/2023_f2e_1/static/css/main.f37b81ab.chunk.css"
+  },
+  {
+    "revision": "966bbb4127bf45557e39",
+    "url": "/2023_f2e_1/static/js/2.e59b1661.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/2023_f2e_1/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "0e449d00275552e235f0",
+    "url": "/2023_f2e_1/static/js/main.3e2b14d3.chunk.js"
+  },
+  {
+    "revision": "1930f6038892f05895d0",
+    "url": "/2023_f2e_1/static/js/runtime~main.6ea2b0ce.js"
+  },
+  {
+    "revision": "540b322f7d708b43d8ed90992012700a",
+    "url": "/2023_f2e_1/static/media/Img-活動01.540b322f.png"
+  },
+  {
+    "revision": "722452bed30d7b86068db79686e99beb",
+    "url": "/2023_f2e_1/static/media/Img-活動02.722452be.png"
+  },
+  {
+    "revision": "da5745c4aab5037e1b26a6b3b90a55cc",
+    "url": "/2023_f2e_1/static/media/Img-活動03.da5745c4.png"
+  },
+  {
+    "revision": "a4ee55b412c13458ec428c1f6821e283",
+    "url": "/2023_f2e_1/static/media/NotoSansTC-Bold.a4ee55b4.ttf"
+  },
+  {
+    "revision": "ad18e8fc265252917dd3698e268977d7",
+    "url": "/2023_f2e_1/static/media/NotoSansTC-Regular.ad18e8fc.ttf"
+  },
+  {
+    "revision": "b2bc92910c411ec78309a83ab15b8372",
+    "url": "/2023_f2e_1/static/media/banner-profile.b2bc9291.png"
+  },
+  {
+    "revision": "4cf488692b0f1a4a69670dd68b4d0f97",
+    "url": "/2023_f2e_1/static/media/bg-小額捐款.4cf48869.png"
+  }
+]);
